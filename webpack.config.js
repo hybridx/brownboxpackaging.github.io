e182cf8b7f5c8de4737efa0686a790162302eb5a
hybridx <hybridx@gmail.com>
2022-02-06 00:45:51 +0530

    Adding ico files

--- webpack.config.js
+++ webpack.config.js
@@ -50,7 +50,7 @@ module.exports = {
                   ],
             },
             {
-                test: /\.(svg|png|jpe?g|gif)$/i,
+                test: /\.(svg|png|ico|jpe?g|gif)$/i,
                 use: [
                     {
                         loader: 'file-loader',
